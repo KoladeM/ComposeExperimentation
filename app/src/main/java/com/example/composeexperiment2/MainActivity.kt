6cb0fda18e3cd06b6e3f841b1b0a428c32b6cff2
koladem <arisekolakolade@gmail.com>
2021-01-15 04:55:29 +0000

    compose preview

--- app/src/main/java/com/example/composeexperiment2/MainActivity.kt
+++ app/src/main/java/com/example/composeexperiment2/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.composeexperiment2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeexperiment2.ui.theme.ComposeExperiment2Theme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,4 +19,10 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun Greeting(name: String) {
     BasicText(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Android")
 }
