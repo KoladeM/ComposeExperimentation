364e57f1a159db46632dd40d65022f7186a55558
koladem <arisekolakolade@gmail.com>
2021-01-15 05:47:10 +0000

    compose adding column image, padding, spacing

--- app/src/main/java/com/example/composeexperiment2/MainActivity.kt
+++ app/src/main/java/com/example/composeexperiment2/MainActivity.kt
@@ -2,27 +2,47 @@ package com.example.composeexperiment2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    BasicText(text = "Hello $name!")
+fun NewsStory() {
+
+    val image = imageResource(R.drawable.header)
+
+    Column(modifier = Modifier.padding(16.dp)){
+        val imageModifier = Modifier
+            .preferredHeight(180.dp)
+            .fillMaxWidth()
+        Image(image, imageModifier, contentScale = ContentScale.Crop)
+        Spacer(Modifier.preferredHeight(16.dp))
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+
 }
 
 @Preview
 @Composable
-fun PreviewGreeting() {
-    Greeting("Android")
+fun DefaultPreview() {
+    NewsStory()
 }
